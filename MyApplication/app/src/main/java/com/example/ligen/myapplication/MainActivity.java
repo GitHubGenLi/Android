137d78a17347f5d122ce4e632b3f3016e0a47e5a
Eric Li <genli1981@hotmail.com>
2016-10-11 00:34:52 +0800

    init

---
+++ MyApplication/app/src/main/java/com/example/ligen/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.ligen.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+public class MainActivity extends AppCompatActivity {
+    MapView mMapView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        if(mMapView != null)
+        mMapView.onDestroy();
+    }
+    @Override
+    protected  void onResume()
+    {
+        super.onResume();
+        if(mMapView != null)
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        if(mMapView != null)
+        mMapView.onPause();
+    }
+}
